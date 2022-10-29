507d3430397e1195587a956b8c5cb9be65f6a755
Anastasia Orlova <anastasia2804@gmail.com>
2022-10-28 21:20:54 -0400

    frontend setup in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import StripeContainer from './components/StripeContainer';
+import product from "./assets/pillow-covers.jpg";
+import { useState } from 'react';
 
 function App() {
+
+  const [showItem, setShowItem] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Boho Design Store</h1>
+     {showItem ? <StripeContainer /> : <> <h3>$10.00</h3> <img src={product} alt="pillow-covers" height={500}/>
+     <button onClick={()=> setShowItem(true)}>Purchase item</button>
+     </>}
     </div>
   );
 }
